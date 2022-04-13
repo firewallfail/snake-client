3773e5f4ffa9e100995510e58ff59d89924ed51a
firewallfail <firewall.fail@hotmail.com>
2022-04-13 09:34:32 -0400

    move input handling to its own file and refactored a little

--- play.js
+++ play.js
@@ -1,22 +1,9 @@
 //ES6 Shorthand
-const { connect } = require('./client')
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
+
+
 setupInput();
