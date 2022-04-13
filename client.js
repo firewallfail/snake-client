8a26a54afa57ea5eeac618afa888bd90cb433aca
firewallfail <firewall.fail@hotmail.com>
2022-04-12 20:19:40 -0400

    Now sends initials to the server

--- client.js
+++ client.js
@@ -6,6 +6,11 @@ const connect = function() {
     host: '172.23.174.163', // IP address here,
     port: '50541' // PORT number here,
   });
+  conn.on("connect", () => {
+    //sssnake puns
+    console.log("Sssuccesssfully connected to game ssserver");
+    conn.write("Name: BHH");
+  });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
