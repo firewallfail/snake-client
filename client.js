0ce7db71e9a13f0ad9053b7490bf8b50bfeea642
firewallfail <firewall.fail@hotmail.com>
2022-04-13 09:29:41 -0400

    added some user input handling to exit the game

--- client.js
+++ client.js
@@ -1,5 +1,21 @@
 const net = require("net");
 
+const handleUserInput = function() {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
@@ -9,7 +25,9 @@ const connect = function() {
   conn.on("connect", () => {
     //sssnake puns
     console.log("Sssuccesssfully connected to game ssserver");
-    conn.write("Name: BHH");
+    conn.write("Name: MUG");
+    // conn.write("Move: up");
+    // setTimeout(() => conn.write("Move: up"), 50);
   });
 
   // interpret incoming data as text
